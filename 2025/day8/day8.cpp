a90b1c65c3241326bc9ca58103260e7dd505cc36
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2025-12-09 01:02:37 -0600

    Day 8 part 1 actually complete this time

--- 2025/day8/day8.cpp
+++ 2025/day8/day8.cpp
@@ -138,8 +138,8 @@ int main() {
 
     int connections = 1000;
 
-    std::vector<Point> points;
-    std::vector<int> circ_point_counts;
+    std::vector<Point*> points;
+    std::unordered_map<int, std::vector<Point*>> circuits;
     std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> pairs;
 
     // Open input file
@@ -153,63 +153,80 @@ int main() {
             int first_comma_pos = line.find(',');
             int second_comma_pos = line.find(',', first_comma_pos + 1);
             // Create point
-            const Point p = Point(
+            Point* p = new Point(
                 std::stoi(line.substr(0, first_comma_pos)),
                 std::stoi(line.substr(first_comma_pos + 1, second_comma_pos - first_comma_pos - 1)),
                 std::stoi(line.substr(second_comma_pos + 1)),
                 ++circuit_id
             );
-            circ_point_counts.push_back(1);
+            // Create circuit with same ID
+            std::vector<Point*> circ = { p };
+            circuits[circuit_id] = circ;
             points.push_back(p);
         }
 
         // Create pairs of points and put them into a min-heap by distance
         for (int p1 = 0; p1 < points.size(); p1++) {
             for (int p2 = p1 + 1; p2 < points.size(); p2++) {      
-                pairs.push(Pair(&points[p1], &points[p2], points[p1].distance(points[p2])));
+                pairs.push(Pair(points[p1], points[p2], (*points[p1]).distance(*points[p2])));
             }
         }
 
         // Connect the "connections" closest pairs
         for (int c = 0; c < connections; c++) {
             Pair close_pair = pairs.top();
-            std::cout << close_pair << '\n';
             // If this pair is not yet part of the same circuit, connect (combine) their circuits
             if (close_pair.point_1->circ_id != close_pair.point_2->circ_id) {
-                // Circuit with more boxes should get increased, other circuit goes to 0
-                if (circ_point_counts[close_pair.point_1->circ_id] >= circ_point_counts[close_pair.point_2->circ_id]) {
-                    circ_point_counts[close_pair.point_1->circ_id] += circ_point_counts[close_pair.point_2->circ_id];
-                    circ_point_counts[close_pair.point_2->circ_id] = 0;
-                    close_pair.point_2->circ_id = close_pair.point_1->circ_id;
-                    std::cout << "Connected! Circuit " << close_pair.point_1->circ_id << " now has " << circ_point_counts[close_pair.point_1->circ_id] << " boxes.\n";
+                // Larger circuit absorbs the smaller circuit
+                int l_cid;
+                int s_cid;
+                if (circuits[close_pair.point_1->circ_id].size() >= circuits[close_pair.point_2->circ_id].size()) {
+                    l_cid = close_pair.point_1->circ_id;
+                    s_cid = close_pair.point_2->circ_id;
                 } else {
-                    circ_point_counts[close_pair.point_2->circ_id] += circ_point_counts[close_pair.point_1->circ_id];
-                    circ_point_counts[close_pair.point_1->circ_id] = 0;
-                    close_pair.point_1->circ_id = close_pair.point_2->circ_id;
-                    std::cout << "Connected! Circuit " << close_pair.point_2->circ_id << " now has " << circ_point_counts[close_pair.point_2->circ_id] << " boxes.\n";
+                    l_cid = close_pair.point_2->circ_id;
+                    s_cid = close_pair.point_1->circ_id;
                 }
+
+                // Move points from the smaller circuit to the bigger circuit
+                for (int p = 0; p < circuits[s_cid].size(); p++) {
+                    circuits[s_cid][p]->circ_id = l_cid;
+                    circuits[l_cid].push_back(circuits[s_cid][p]);
+                }
+
+                // Empty out and discard the smaller circuit
+                circuits[s_cid].clear();
+                circuits.erase(s_cid);
             }
-            std::cout << '\n';
             pairs.pop();
         }
 
-        // Sort circuits by number of points (descending)
-        std::sort(circ_point_counts.begin(), circ_point_counts.end(), std::greater<int>());
-
-        for (int i = 0; i < circ_point_counts.size(); i++) {
-            if (circ_point_counts[i] > 0) {
-                std::cout << circ_point_counts[i] << '\n';
+        // Get largest 3 circuit sizes
+        int l = 0;
+        int sl = 0;
+        int tl = 0;
+        for (const auto & pair : circuits) {
+            if (pair.second.size() >= l) {
+                tl = sl;
+                sl = l;
+                l = pair.second.size();
+            } else if (pair.second.size() >= sl) {
+                tl = sl;
+                sl = pair.second.size();
+            } else if (pair.second.size() >= tl) {
+                tl = pair.second.size();
             }
         }
-        std::cout << '\n';
 
         // Multiply together the size of the 3 largest circuits
-        long result = 1;
-        for (int i = 0; i < 3 && i < circ_point_counts.size(); i++) {
-            result = result * circ_point_counts[i];
-        }
+        long result = l * sl * tl;
 
         std::cout << "Result: " << result << std::endl;
+
+        // Delete allocated points
+        for (int i = 0; i < points.size(); i++) {
+            delete points[i];
+        }
 
         input_file.close();
     } else {
