72d0f315f7115ad4ba090e950f72419e4b8a2b54
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2025-12-09 15:38:46 -0600

    Add day 9 notes

--- 2025/day9/day9.cpp
+++ 2025/day9/day9.cpp
@@ -6,7 +6,24 @@
 #include <unordered_map>
 
 /*
-    
+    Slide down the firepole in the corner and land in the North Pole movie theater.
+
+    Big tile floor with an interesting pattern.  Elves are redecorating by switching
+    out some of the square tiles in the big grid they form.  Some of the tiles are
+    red. The elves would like to find the largest rectangle that uses red tiles for
+    two of its opposite corners.
+
+    They have a list of where the red tiles are located in the grid (puzzle input).
+
+    Example given.
+
+    You can choose any two red tiles as the opposite corners of the rectange. The
+    goal then is to find the largest rectangle possible.
+
+    There may be multiple largest rectangles with the same area, keep in mind.
+
+    Using two red tiles as opposite corners, what is the largest area of any rectangle
+    you could make?
 */
 
 int main() {
