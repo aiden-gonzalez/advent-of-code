fbc64d3ff5de05492b7a5e7271a4f7d4255a3202
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2025-12-14 00:45:41 -0600

    Day 10 files created

---
+++ 2025/day10/day10.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+/*
+    
+*/
+
+int main() {
+    std::string line;
+    std::ifstream input_file;
+
+    // Open input file
+    input_file.open("example_input.txt");
+
+    if (input_file.is_open()) {
+        // Read line by line
+        while (getline(input_file, line)) {
+
+        }
+
+        input_file.close();
+    } else {
+        std::cout << "Unable to open file";
+        return 1;
+    }
+
+    return 0;
+}
