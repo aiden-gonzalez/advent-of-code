d6c4a7fe8686bc5848a0ab0e509ce4e838f57624
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2025-12-24 22:29:16 -0600

    Day 11 trying to input file

--- 2025/day11/day11.cpp
+++ 2025/day11/day11.cpp
@@ -3,12 +3,39 @@
 #include <string>
 
 /*
-    
+    There are devices with names. And each line of the input says what other devices
+    they are attached to.  And they just want to know how many paths take you from
+    "you" (the root) to "out" (the end).
+
+    Graph and traversal. Lets do it.
 */
+
+class Node {
+    public:
+        Node (std::string nm) {
+            name = nm;
+        }
+
+        std::string name;
+        std::vector<Node*> cables;
+
+        // Friend function: stream operator overload
+        friend std::ostream & operator<<(std::ostream & os, const Node & n);
+};
+
+std::ostream & operator<<(std::ostream & os, Node const & n) {
+    os << n.name << ':';
+    for (int i = 0; i < n.cables.size(); i++) {
+        os << ' ' << n.cables[i]->name;
+    }
+    return os;
+} 
 
 int main() {
     std::string line;
     std::ifstream input_file;
+
+    std::unordered_map<std::string, Node> nodes;
 
     // Open input file
     input_file.open("example_input.txt");
@@ -16,7 +43,33 @@ int main() {
     if (input_file.is_open()) {
         // Read line by line
         while (getline(input_file, line)) {
-            
+            // First just fill up list of nodes
+            std::string node_name = line.substr(0, line.find(':'));
+            nodes[node_name] = Node(node_name);
+        }
+
+        // Go back to beginning of file
+        input_file.clear();
+        input_file.seekg(0);
+
+        // Now read in cable connections
+        while (getline(input_file, line)) {
+            Node root = nodes.at(line.substr(0, line.find(':')));
+            int node_start = line.find(' ') + 1;
+            int node_end = line.find(' ', node_start) - 1;
+            do {
+                // Grab node name and add the address of that node to cable connections
+                std::string node_name = line.substr(node_start, node_end - node_start + 1);
+                root.cables.push_back(&nodes.at(node_name));
+
+                // Identify next node
+                node_start = node_end + 2;
+                node_end = line.find(' ', node_start) - 1;
+            } while (node_start < line.size());
+        }
+
+        for (const auto& node : nodes) {
+            std::cout << node.second << '\n';
         }
 
         input_file.close();
