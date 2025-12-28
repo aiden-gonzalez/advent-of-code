f943c4301085b53a070972b13239d920505b0f4c
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2025-12-27 23:37:34 -0600

    Day 12 read input and generate orientations

--- 2025/day12/day12.cpp
+++ 2025/day12/day12.cpp
@@ -69,7 +69,73 @@ class Shape_Grid {
 std::vector<Shape_Grid> generate_orientations(Shape_Grid g) {
     // TODO generate orientations
     std::vector<Shape_Grid> orientations;
+
+    // Original
     orientations.push_back(g);
+
+    // Flip vertical
+    Shape_Grid g1 = g;
+    for (int i = 0; i < g.grid.size(); i++) {
+        for (int j = 0; j < g.grid[i].size(); j++) {
+            g1.grid[g.grid.size()-1-i][j] = g.grid[i][j];
+        }
+    }
+    orientations.push_back(g1);
+
+    // Flip horizontal
+    Shape_Grid g2 = g;
+    for (int i = 0; i < g.grid.size(); i++) {
+        for (int j = 0; j < g.grid[i].size(); j++) {
+            g2.grid[i][g.grid[i].size()-1-j] = g1.grid[i][j];
+        }
+    }
+    orientations.push_back(g2);
+
+    // Flip vertical
+    Shape_Grid g3 = g;
+    for (int i = 0; i < g.grid.size(); i++) {
+        for (int j = 0; j < g.grid[i].size(); j++) {
+            g3.grid[g.grid[i].size()-1-i][j] = g2.grid[i][j];
+        }
+    }
+    orientations.push_back(g3);
+
+    // Rotate 90 ccw
+    Shape_Grid g4 = g;
+    for (int i = 0; i < g.grid.size(); i++) {
+        for (int j = 0; j < g.grid[i].size(); j++) {
+            g4.grid[j][i] = g3.grid[i][j];
+        }
+    }
+    orientations.push_back(g4);
+
+    // Flip vertical
+    Shape_Grid g5 = g;
+    for (int i = 0; i < g.grid.size(); i++) {
+        for (int j = 0; j < g.grid[i].size(); j++) {
+            g5.grid[g.grid[i].size()-1-i][j] = g4.grid[i][j];
+        }
+    }
+    orientations.push_back(g5);
+
+    // Flip horizontal
+    Shape_Grid g6 = g;
+    for (int i = 0; i < g.grid.size(); i++) {
+        for (int j = 0; j < g.grid[i].size(); j++) {
+            g6.grid[i][g.grid[i].size()-1-j] = g5.grid[i][j];
+        }
+    }
+    orientations.push_back(g6);
+
+    // Flip vertical
+    Shape_Grid g7 = g;
+    for (int i = 0; i < g.grid.size(); i++) {
+        for (int j = 0; j < g.grid[i].size(); j++) {
+            g7.grid[g.grid[i].size()-1-i][j] = g6.grid[i][j];
+        }
+    }
+    orientations.push_back(g7);
+
     return orientations;
 }
 
@@ -78,8 +144,8 @@ class Shape {
         Shape(int i, std::vector<std::vector<bool>> sg) {
             id = i;
             grid = sg;
-            orientations.push_back(sg);
-            // orientations = generate_orientations(sg);
+            // orientations.push_back(sg);
+            orientations = generate_orientations(sg);
         }
 
         int id;
@@ -129,7 +195,6 @@ int main() {
             int idx;
             if (colon != std::string::npos) {
                 idx = std::stoi(line.substr(0,colon));
-                std::cout << idx << " next shape dectected\n";
             }
 
             // Read present shape
@@ -138,61 +203,55 @@ int main() {
             int empty = line.find('.');
             int i = 0;
             // Find the lines relating to the present shape
-            while (taken != std::string::npos || empty != std::string::npos) {
-                std::cout << "reading present shape " << idx << "\n";
-                std::string ln = line.substr(0, line.size());
-                for (int j = 0; j < ln.size(); j++) {
-                    std::cout << ln.size() << "\n";
-                    std::cout << ln[j] << "\n";
-                    if (ln[j] == '#') {
-                        shape[i][j]=(true);
-                    } else if (ln[j] == '.') {
-                        shape[i][j]=(false);
+            if (taken != std::string::npos || empty != std::string::npos) {
+                while (taken != std::string::npos || empty != std::string::npos) {
+                    std::string ln = line.substr(0, line.size());
+                    for (int j = 0; j < ln.size(); j++) {
+                        if (ln[j] == '#') {
+                            shape[i][j]=(true);
+                        } else if (ln[j] == '.') {
+                            shape[i][j]=(false);
+                        }
                     }
-                    std::cout << "just assigned: " << shape[i][j] << "\n";
+                    i++;
+                    getline(input_file, line);
+                    taken = line.find('#');
+                    empty = line.find('.');
                 }
-                std::cout << "\n";
-                i++;
-                getline(input_file, line);
-                taken = line.find('#');
-                empty = line.find('.');
+                shapes.push_back(Shape(idx,shape));
+                continue;
             }
-            shapes.push_back(Shape(idx,shape));
-            std::cout << "psnext \n";
-            // print_shape(shapes[i].orientations[0]);
-            
+
             // Read region shape
             int row;
             int col;
             std::vector<int> shape_quantity;
             int x = line.find('x');
             if (x != std::string::npos) {
-                std::cout << "reading region shape\n";
                 row = std::stoi(line.substr(0,x));
                 col = std::stoi(line.substr(x + 1,colon));
-                std::cout << row << " r | c " << col << "\n";
                 int last_space = 0;
                 int current_space = line.find(' ',last_space);
                 // Read the number of each present desired for the region
                 while (current_space != std::string::npos && current_space < line.size()) {
-                    std::cout << "while" << current_space << " ls " << last_space << "\n";
                     last_space = current_space + 1;
                     shape_quantity.push_back(std::stoi(line.substr(last_space, 1)));
                     current_space = line.find(' ',last_space);
                 }
+                regions.push_back(Region(row,col,shape_quantity));
             }
-            regions.push_back(Region(row,col,shape_quantity));
         }
 
         // Print what was read
         for (Shape s : shapes) {
             std::cout << "Shape Index: " << s.id << "\n";
-            for (int i = 0; i < s.grid.size(); i++) {
+            for (int i = 0; i < s.orientations.size(); i++) {
+                std::cout << "Orientation " << i << "\n";
                 print_shape(s.orientations[i]);
             }
         }
         for (Region r : regions) {
-            std::cout << "Rows: " << r.height << " Cols: " << r.width;
+            std::cout << "Rows: " << r.height << " Cols: " << r.width << " Present Counts: ";
             for (int i = 0; i < r.shape_counts.size(); i++) {
                 std::cout << " " << r.shape_counts[i];
             }
