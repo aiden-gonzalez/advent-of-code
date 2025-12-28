9acc4d1e52fc59f71a660aaa80b4f2e20c785655
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2025-12-28 17:27:36 -0600

    Day 12 clean up

--- 2025/day12/day12.cpp
+++ 2025/day12/day12.cpp
@@ -57,81 +57,75 @@ ___________________________________________
 
 */
 
-class Shape_Grid {
-    public:
-        Shape_Grid(std::vector<std::vector<bool>> g) {
-            grid = g;
-        }
+// Alias for shape_grid
+using ShapeGrid = std::vector<std::vector<bool>>;
 
-        std::vector<std::vector<bool>> grid;
-};
-
-std::vector<Shape_Grid> generate_orientations(Shape_Grid g) {
-    // TODO generate orientations
-    std::vector<Shape_Grid> orientations;
+std::vector<ShapeGrid> generate_orientations(ShapeGrid g) {
+    // TODO simplify this with helper function
+    std::vector<ShapeGrid> orientations;
 
     // Original
     orientations.push_back(g);
 
     // Flip vertical
-    Shape_Grid g1 = g;
-    for (int i = 0; i < g.grid.size(); i++) {
-        for (int j = 0; j < g.grid[i].size(); j++) {
-            g1.grid[g.grid.size()-1-i][j] = g.grid[i][j];
+    ShapeGrid g1 = g;
+    for (int i = 0; i < g.size(); i++) {
+        for (int j = 0; j < g[i].size(); j++) {
+            g1[g.size()-1-i][j] = g[i][j];
         }
     }
     orientations.push_back(g1);
 
     // Flip horizontal
-    Shape_Grid g2 = g;
-    for (int i = 0; i < g.grid.size(); i++) {
-        for (int j = 0; j < g.grid[i].size(); j++) {
-            g2.grid[i][g.grid[i].size()-1-j] = g1.grid[i][j];
+    ShapeGrid g2 = g;
+    for (int i = 0; i < g.size(); i++) {
+        for (int j = 0; j < g[i].size(); j++) {
+            g2[i][g[i].size()-1-j] = g1[i][j];
         }
     }
     orientations.push_back(g2);
 
     // Flip vertical
-    Shape_Grid g3 = g;
-    for (int i = 0; i < g.grid.size(); i++) {
-        for (int j = 0; j < g.grid[i].size(); j++) {
-            g3.grid[g.grid[i].size()-1-i][j] = g2.grid[i][j];
+    ShapeGrid g3 = g;
+    for (int i = 0; i < g.size(); i++) {
+        for (int j = 0; j < g[i].size(); j++) {
+            g3[g[i].size()-1-i][j] = g2[i][j];
         }
     }
     orientations.push_back(g3);
 
     // Rotate 90 ccw
-    Shape_Grid g4 = g;
-    for (int i = 0; i < g.grid.size(); i++) {
-        for (int j = 0; j < g.grid[i].size(); j++) {
-            g4.grid[j][i] = g3.grid[i][j];
+    ShapeGrid g4 = g;
+    for (int i = 0; i < g.size(); i++) {
+        for (int j = 0; j < g[i].size(); j++) {
+            g4[j][i] = g3[i][j];
         }
     }
     orientations.push_back(g4);
 
     // Flip vertical
-    Shape_Grid g5 = g;
-    for (int i = 0; i < g.grid.size(); i++) {
-        for (int j = 0; j < g.grid[i].size(); j++) {
-            g5.grid[g.grid[i].size()-1-i][j] = g4.grid[i][j];
+    ShapeGrid g5 = g;
+    for (int i = 0; i < g.size(); i++) {
+        for (int j = 0; j < g[i].size(); j++) {
+            g5[g[i].size()-1-i][j] = g4[i][j];
         }
     }
     orientations.push_back(g5);
 
     // Flip horizontal
-    Shape_Grid g6 = g;
-    for (int i = 0; i < g.grid.size(); i++) {
-        for (int j = 0; j < g.grid[i].size(); j++) {
-            g6.grid[i][g.grid[i].size()-1-j] = g5.grid[i][j];
+    ShapeGrid g6 = g;
+    for (int i = 0; i < g.size(); i++) {
+        for (int j = 0; j < g[i].size(); j++) {
+            g6[i][g[i].size()-1-j] = g5[i][j];
         }
     }
     orientations.push_back(g6);
 
     // Flip vertical
-    Shape_Grid g7 = g;
-    for (int i = 0; i < g.grid.size(); i++) {
-        for (int j = 0; j < g.grid[i].size(); j++) {
-            g7.grid[g.grid[i].size()-1-i][j] = g6.grid[i][j];
+    ShapeGrid g7 = g;
+    for (int i = 0; i < g.size(); i++) {
+        for (int j = 0; j < g[i].size(); j++) {
+            g7[g[i].size()-1-i][j] = g6[i][j];
         }
     }
     orientations.push_back(g7);
@@ -141,16 +135,15 @@ std::vector<Shape_Grid> generate_orientations(Shape_Grid g) {
 
 class Shape {
     public:
-        Shape(int i, std::vector<std::vector<bool>> sg) {
+        Shape(int i, ShapeGrid sg) {
             id = i;
             grid = sg;
-            // orientations.push_back(sg);
             orientations = generate_orientations(sg);
         }
 
         int id;
-        std::vector<std::vector<bool>> grid;
-        std::vector<Shape_Grid> orientations;
+        ShapeGrid grid;
+        std::vector<ShapeGrid> orientations;
 };
 
 class Region {
@@ -159,17 +152,20 @@ class Region {
             height = h;
             width = w;
             shape_counts = sc;
+            grid = ShapeGrid(h, std::vector<bool>(w,0));
         }
 
         int height;
         int width;
         std::vector<int> shape_counts;
+        std::vector<std::vector<bool>> grid;
 };
 
-void print_shape(Shape_Grid sg) {
-    for (int i = 0; i < sg.grid.size(); i++) {
-        for (int j = 0; j < sg.grid[i].size(); j++) {
-            if (sg.grid[i][j]) {
+// Print a shape grid with # for true and . for false
+void print_shape(ShapeGrid sg) {
+    for (int i = 0; i < sg.size(); i++) {
+        for (int j = 0; j < sg[i].size(); j++) {
+            if (sg[i][j]) {
                 std::cout << "#";
             } else {
                 std::cout << ".";
@@ -177,6 +173,61 @@ void print_shape(Shape_Grid sg) {
         }
         std::cout << "\n";
     }
+}
+
+// Put a present in a part of the grid starting from row and col on the grid
+bool place_present(Region region, ShapeGrid sg, int row, int col) {
+    // Check if the present would go outside the region grid
+    if (row + sg.size() >= region.grid.size() || col + sg[0].size() >= region.grid[0].size()) {
+        return false;
+    }
+
+    for (int r = 0; r < sg.size(); r++) {
+        for (int c = 0; c < sg[r].size(); c++) {
+            if (region.grid[row][col]) {
+                std::cout << row << " r | c " << col << " FALSE\n";
+            } else {
+                std::cout << row << " r | c " << col << " TRUE\n";
+            }
+            region.grid[row][col] = sg[r][c];
+        }
+    }
+
+    return true;
+}
+
+// Solve for if the region can fit the shapes needed
+int fit_region(Region region, std::vector<Shape> shapes) {
+    // In this region, find out which shapes and how many need to fit
+    for (int i = 0; i < region.shape_counts.size(); i++) {
+        std::cout << "i:" << i << " ";
+
+        // How many of this shape is needed
+        int sc = region.shape_counts[i];
+        if (sc == 0) {
+            std::cout << "skip\n";
+            continue;
+        }
+        std::cout << "sc:" << sc << "\n";
+
+        // Access that shape from the list of shapes
+        Shape s = shapes[i];
+        
+        // Place the present in the region
+        for (int row = 0; row <= region.height - s.grid.size(); row++) {
+            for (int col = 0; col <= region.width - s.grid.size(); col++) {
+                // Try placing the present for every orientation of that shape
+                for (int j = 0; j < s.orientations.size(); j++) {
+                    ShapeGrid sg = s.orientations[j];
+                    print_shape(s.orientations[j]);
+                    place_present(region, s.orientations[j], row, col);
+                }
+            }
+        }
+    }
+    print_shape(region.grid);
+    // Figure out if any combination of one orientation per shape needed can be fit
+    return 0;
 }
 
 int main() {
@@ -252,11 +303,23 @@ int main() {
         }
         for (Region r : regions) {
             std::cout << "Rows: " << r.height << " Cols: " << r.width << " Present Counts: ";
-            for (int i = 0; i < r.shape_counts.size(); i++) {
-                std::cout << " " << r.shape_counts[i];
+            for (int sc : r.shape_counts) {
+                std::cout << " " << sc;
             }
             std::cout << "\n";
+            print_shape(r.grid);
         }
+
+        std::cout << "Find Solution:\n";
+        // Fit presents into region and sum up the number of regions for which all presents fit
+        int sum = 0;
+        for (Region r : regions) {
+            sum += fit_region(r,shapes);
+            return 0;
+        }
+
+        std::cout << "\nTOTAL: " << sum << " Regions Fit \n";
+
         input_file.close();
     } else {
         std::cout << "Unable to open file";
